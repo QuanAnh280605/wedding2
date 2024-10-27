81ed33c610374d2126dd2e32b90fbe1453b775a7
Quannguyen <tmi.quanna@gmail.com>
2024-10-27 12:54:45 +0700

    commit 3

---
+++ src/router/router.js
@@ -0,0 +1,37 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+import About from '../views/About.vue';
+import Login from '../auth/login.vue';
+import Resgister from '../auth/register.vue';
+import App from '../layouts/App.vue';
+
+
+const routes = [
+    {
+        path: '/',
+        name: 'App',
+        component:App,
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About,
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: Login,
+    },
+    {
+        path: '/register',
+        name: 'resgister',
+        component: Resgister,
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
